8b9a4c82a9618006e6febead1abe3e44368ade86
mi991203 <sh19991203>
2022-08-22 08:30:49 +0800

    admin模块基础代码编写

--- e-commerce-admin/src/main/java/com/shao/SpringBootAdminApplication.java
+++ e-commerce-admin/src/main/java/com/shao/SpringBootAdminApplication.java
@@ -1,13 +1,13 @@
 package com.shao;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
+@EnableAdminServer
 @SpringBootApplication
-public class NacosClientApplication {
+public class SpringBootAdminApplication {
     public static void main(String[] args) {
-        SpringApplication.run(NacosClientApplication.class, args);
+        SpringApplication.run(SpringBootAdminApplication.class, args);
     }
 }
