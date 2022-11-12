eb9903a9788988ea2fb8106fa3023d5d0492242b
ShaoHong <sh1101315853@163.com>
2022-11-12 14:46:00 +0800

    Token统一拦截处理和Swagger2相关代码编写

---
+++ e-commerce-service/e-commerce-service-config/src/main/java/com/shao/conf/WebMvcConfig.java
@@ -0,0 +1,37 @@
+package com.shao.conf;
+
+
+import com.shao.filter.LoginUserInfoInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    /**
+     * 添加拦截器配置
+     * @param registry 拦截注册器
+     */
+    @Override
+    protected void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginUserInfoInterceptor()).addPathPatterns("/**").order(0);
+    }
+
+    /**
+     * <h2>让 MVC 加载 Swagger 的静态资源</h2>
+     * */
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/**").
+                addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("doc.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
+    }
+}
